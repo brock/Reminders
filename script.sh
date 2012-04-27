bfd71d4518e5d59f5f550d47e2899114cd1e4f7c
Brock Angelo <brock@voceconnect.com>
2012-04-27 16:52:18 -0500

    addition of 'remindme cleanup' command

--- script.sh
+++ script.sh
@@ -3,6 +3,26 @@
 # Convert minutes to seconds, then create a one-time cron that
 # simply calls up a sticky growlnotify with your reminder. 
 
+
+if [[ $1 == "cleanup" ]]; then
+
+	echo "Cleaning up expired reminders..."
+
+	if launchctl list | grep com.approductive.remindersapp > /dev/null; then
+		ACTIVE_REMINDERS=(`launchctl list | grep com.approductive.remindersapp | awk '{print $3}'`)
+		REMINDER_PLISTS=(`ls ~/Library/LaunchAgents/com.approductive.remindersapp.*`)
+		for plist in "${REMINDER_PLISTS[@]}"
+		do
+			trimmed_plist=$(basename $plist | sed 's#.plist##g')
+			match=$(echo "${ACTIVE_REMINDERS[@]}" | grep -o "$trimmed_plist")
+			if [[ -z $match ]]; then
+				rm $plist
+			fi
+		done
+	fi
+	exit
+fi
+
 MINUTES=$1
 TIMESTAMP=$(command date +%s)
 TIMER=$(($1 * 60))
